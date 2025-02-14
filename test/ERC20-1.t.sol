715611a15932be7abe89bed28baba650f8338b85
mooji <70057651+moooooji@users.noreply.github.com>
2025-02-14 09:23:26 +0900

    initial commit

--- test/ERC20-1.t.sol
+++ test/ERC20-1.t.sol
@@ -1,37 +1,37 @@
-// SPDX-License-Identifier: MIT
+// // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+// pragma solidity ^0.8.0;
 
-import "../lib/forge-std/src/Test.sol";
-import "../src/ERC20.sol";
+// import "../lib/forge-std/src/Test.sol";
+// import "../src/ERC20.sol";
 
-contract UpsideTokenTest is Test {
-    address internal constant alice = address(1);
-    address internal constant bob = address(2);
+// contract UpsideTokenTest is Test {
+//     address internal constant alice = address(1);
+//     address internal constant bob = address(2);
 
-    ERC20 upside_token;
+//     ERC20 upside_token;
 
-    function setUp() public {
-        upside_token = new ERC20("UPSIDE", "UPS");
-        upside_token.transfer(alice, 50 ether);
-        upside_token.transfer(bob, 50 ether);
-    }
+//     function setUp() public {
+//         upside_token = new ERC20("UPSIDE", "UPS");
+//         upside_token.transfer(alice, 50 ether);
+//         upside_token.transfer(bob, 50 ether);
+//     }
     
-    function testFailPauseNotOwner() public {
-        vm.prank(alice);
-        upside_token.pause();
-    }
+//     function testFailPauseNotOwner() public {
+//         vm.prank(alice);
+//         upside_token.pause();
+//     }
 
-    function testFailTransfer() public {
-        upside_token.pause();
-        vm.prank(alice);
-        upside_token.transfer(bob, 10 ether);
-    }
+//     function testFailTransfer() public {
+//         upside_token.pause();
+//         vm.prank(alice);
+//         upside_token.transfer(bob, 10 ether);
+//     }
 
-    function testFailTransferFrom() public {
-        upside_token.pause();
-        vm.prank(alice);
-        upside_token.approve(msg.sender, 10 ether);
-        upside_token.transferFrom(alice, bob, 10 ether);
-    }
-}
+//     function testFailTransferFrom() public {
+//         upside_token.pause();
+//         vm.prank(alice);
+//         upside_token.approve(msg.sender, 10 ether);
+//         upside_token.transferFrom(alice, bob, 10 ether);
+//     }
+// }
